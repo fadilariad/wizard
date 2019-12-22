d4ba1ed6d4c5e8cea5f4a0325bd52309a5717c82
fadilariad <32985210+fadilariad@users.noreply.github.com>
2019-12-22 14:58:35 +0200

    Add files via upload

---
+++ src/adress.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Adress extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <form>
+
+            </form>
+        );
+    }
+}
+
+export default Adress
