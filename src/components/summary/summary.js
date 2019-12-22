a3d8cf73fa9f0751b29a2e4f39b8d95ea92cc35b
fadilariad <fadilariad@gmail.com>
2019-12-22 17:31:58 +0200

    change summary.js path

--- src/components/summary/summary.js
+++ src/components/summary/summary.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-class Summary extends React.Component{
-    constructor(props) {
-        super(props);
-    }
-
+class Summary extends React.Component {
     render() {
         return (
-            <form>
+            <div>
+                <div className={"page"}>
+                    <div className={"img-wrapper"}>
+                        <div
+                            className={"img"}
+                            style={{
+                                backgroundImage: `url(${localStorage.getItem("image")})`
+                            }}
+                        >
 
-            </form>
+                        </div>
+                        <div className={"details-wrapper"}>
+                            <h1>{localStorage.getItem("name")}</h1>
+                            <h2>{localStorage.getItem("email")}</h2>
+                            <h2>{localStorage.getItem("birth")}</h2>
+
+                            <h2>
+                                {localStorage.getItem("street")}{" "}
+                                {localStorage.getItem("number")}, {localStorage.getItem("city")}
+                            </h2>
+
+                            <h2 className={"title"}>Hobbies:</h2>
+                            <h2>{localStorage.getItem("hobbies")}</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button className={"btn"}>Prev</button>
+                </div>
+            </div>
         );
     }
 }
 
-export default Summary
+export default Summary;
+
+
