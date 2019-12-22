fdd601b3a7fcf5b952874c37c3e9cbd665cf1165
fadilariad <fadilariad@gmail.com>
2019-12-22 15:03:14 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import {Switch} from 'react-router-dom'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <React.Fragment>
+          <Switch>
+            <Route exact path="wizard-personal" component={}/>
+            <Route exact path="/wizard-address" component={}/>
+            <Route exact path="/wizard-profile" component={}/>
+            <Route exact path="/wizard-summary" component={}/>
+          </Switch>
+        </React.Fragment>
+      </Router>
   );
 }
 
---
+++ src/components/personal/personal.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Personal extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <form>
+
+            </form>
+        );
+    }
+}
+
+export default Personal
